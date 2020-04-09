491c347171f45d2669b0d10c48cfdc98c2201783
Marco Sporchia <sporchiamarco@gmail.com>
2020-04-09 21:14:29 +0200

    added event test for task

--- tests/Domain/TaskTest.php
+++ tests/Domain/TaskTest.php
@@ -5,14 +5,23 @@ namespace Tests\Domain;
 
 
 use App\Domain\Base\UuidType;
+use App\Domain\TaskList\Event\Task\TaskHasBeenCreated;
+use App\Domain\TaskList\Event\Task\TaskHasBeenDeleted;
+use App\Domain\TaskList\Event\Task\TaskHasBeenUpdated;
+use App\Domain\TaskList\Event\User\UserHasBeenDeleted;
 use App\Domain\TaskList\Models\Task\Task;
 use App\Domain\TaskList\Models\Task\TaskDescription;
 use App\Domain\TaskList\Models\Task\TaskDueDate;
 use App\Domain\TaskList\Models\Task\TaskId;
 use App\Domain\TaskList\Models\Task\TaskStatus;
+use App\Domain\TaskList\Models\TaskList\TaskList;
 use App\Domain\TaskList\Models\TaskList\TaskListId;
+use App\Domain\TaskList\Models\TaskList\TaskListName;
+use App\Domain\TaskList\Models\User\User;
+use App\Domain\TaskList\Models\User\UserName;
 use DateInterval;
 use DateTime;
+use Event;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
 
@@ -85,7 +94,69 @@ class TaskTest extends TestCase
         // Check the date
         $this->assertTrue($t1->isDueToday());
         $this->assertFalse($t3->isDueToday());
+    }
 
+    public function testEvents()
+    {
+        // We lister for the possible dispatched event, so we can be sure that the User is dispatching event correctly
+        Event::fake([
+            TaskHasBeenCreated::class,
+            TaskHasBeenUpdated::class,
+            TaskHasBeenDeleted::class
+        ]);
+
+        $u1 = User::create(new UserName('M','R'));
+        $l1 = TaskList::create($u1->getId(), new TaskListName('Home Works'));
+        $t1 = Task::create(new TaskDescription('Job 1'),$l1->getId());
+        Event::assertDispatched(TaskHasBeenCreated::class,function ($event) use ($t1) {
+            return $t1->equals($event->task);
+        });
+
+        $t2 = Task::create(new TaskDescription('Job 2'),$l1->getId());
+        Event::assertDispatched(TaskHasBeenCreated::class,function ($event) use ($t2) {
+            return $t2->equals($event->task);
+        });
+
+        Event::assertNotDispatched(TaskHasBeenUpdated::class);
+
+
+
+        new Task(
+            TaskId::generate(),
+            new TaskDescription('test2'),
+            TaskStatus::create(TaskStatus::TODO),
+            new TaskDueDate(new DateTime()),
+            TaskListId::generate()
+        );
+        Event::assertDispatched(TaskHasBeenCreated::class,2); // We didn't create the TaskList, so we should have 2 event ( for the 2 previus creation )
+
+        // No previous "hasBeenUpdated" event fired
+        Event::assertNotDispatched(TaskHasBeenUpdated::class);
+
+        // Check if the name change correctly when we change name
+        $t1->setAsTodo();
+        Event::assertDispatched(TaskHasBeenUpdated::class,function ($event) use ($t1)
+        {
+            return $t1->equals($event->task);
+        });
+        $t1->setAsDone();
+        Event::assertDispatched(TaskHasBeenUpdated::class,function ($event) use ($t1)
+        {
+            return $t1->equals($event->task);
+        });
+
+        $t1->changeDate(new TaskDueDate(new DateTime()));
+        Event::assertDispatched(TaskHasBeenUpdated::class,function ($event) use ($t1)
+        {
+            return $t1->equals($event->task);
+        });
+
+        Event::assertNotDispatched(TaskHasBeenDeleted::class);
+        $t1->delete();
+        Event::assertDispatched(TaskHasBeenDeleted::class,function ($event) use ($t1)
+        {
+            return $t1->equals($event->task);
+        });
     }
 
 }
